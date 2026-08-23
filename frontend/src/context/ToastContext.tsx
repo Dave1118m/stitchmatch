0ea73bbc8bfc72efb2ed2c0132c9e8038df08bde
Dave1118m <dawitdawit221@gmail.com>
2026-08-23 10:15:54 +0300

    chore(cleanup): remove unused components, obsolete scratch scripts, dead utils, test files, and redundant root packages

--- frontend/src/context/ToastContext.tsx
+++ frontend/src/context/ToastContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
-import Toast, { ToastType } from '../components/Toast.tsx';
+import Toast, { ToastType } from '../components/Toast';
 
 interface ToastItem {
   id: string;
